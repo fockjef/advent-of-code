e9474e68b3fcd6450bff2c9fe199744d9fd2211b
jef <fockjef@gmail.com>
2021-12-01 09:00:02 -0600

    sliding windows

---
+++ 2021/day_01.js
@@ -0,0 +1,11 @@
+/* --- Day 1: Report Repair --- */
+
+function day_01a(){
+	let data = parseInput( Number );
+	return data.filter( ( x, i) => i >= 1 && x > data[i-1]).length;
+}
+
+function day_01a(){
+	let data = parseInput( Number );
+	return data.filter( ( x, i) => i >= 3 && x > data[i-3]).length;
+}
