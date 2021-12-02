618749d114fee880f270e1f6359e49f7985c7c52
fockjef <fockjef@gmail.com>
2021-12-02 10:28:36 -0800

    const objects are not accessible via the window object

--- 2021/day_01.js
+++ 2021/day_01.js
@@ -1,5 +1,5 @@
 /* --- Day 1: Sonar Sweep --- */
 
 const compareWindows = (n) => parseInput(Number).filter( ( x, i, data) => i >= n && x > data[i-n]).length;
-const day_01a = () => compareWindows(1)
-const day_01b = () => compareWindows(3)
+var day_01a = () => compareWindows(1)
+var day_01b = () => compareWindows(3)
