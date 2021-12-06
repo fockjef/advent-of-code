d44c589de7d2ef39f30996f2dc5c6f3774f0214a
fockjef <fockjef@users.noreply.github.com>
2021-12-06 16:55:42 -0600

    slight simplification for day 4

--- 2021/day_04.js
+++ 2021/day_04.js
@@ -8,12 +8,11 @@ function day_04a(){
         let num = randNums[i];
         for( let j = 0; j < bingoCards.length; j++ ){
             let card = bingoCards[j],
-                row = card.findIndex( row => row.includes(num));
-            if( row != -1 ){
-                let col = card[row].indexOf(num);
-                card[row][col] = -1;
-                if( card[row].every( n => n == -1) || card.every( row => row[col] == -1) ){
-                    console.log( card, num);
+                row = card.find( row => row.includes(num));
+            if( row ){
+                let col = row.indexOf(num);
+                row[col] = -1;
+                if( row.every( n => n == -1) || card.every( r => r[col] == -1) ){
                     return sum([].concat( ...card).filter( x => x != -1)) * num;
                 }
             }
@@ -29,14 +28,13 @@ function day_04b(){
         let num = randNums[i];
         for( let j = 0; j < bingoCards.length; j++ ){
             let card = bingoCards[j],
-                row = card.findIndex( row => row.includes(num));
-            if( row != -1 ){
-                let col = card[row].indexOf(num);
-                card[row][col] = -1;
-                if( card[row].every( n => n == -1) || card.every( row => row[col] == -1) ){
+                row = card.find( row => row.includes(num));
+            if( row ){
+                let col = row.indexOf(num);
+                row[col] = -1;
+                if( row.every( n => n == -1) || card.every( r => r[col] == -1) ){
                     bingoCards.splice( j--, 1);
                     if( bingoCards.length == 0 ){
-                        console.log( card, num);
                         return sum([].concat( ...card).filter( x => x != -1)) * num;
                     }
                 }
