150b86145f6134abac37d8d99b24bfab6d2cd136
fockjef <fockjef@users.noreply.github.com>
2021-12-06 20:00:55 -0600

    day 6 optimized

--- 2021/day_06.js
+++ 2021/day_06.js
@@ -1,29 +1,18 @@
 /* --- Day 6: Lanternfish --- */
 
-const numFish = (function(){
-    let cache = {};
-    return function( numDays, t0){
-        // check cache for redundant calls
-        let key = `${+t0}:${+numDays}`;
-        if( key in cache ) return cache[key];
-        fish = new Array(9).fill(0);
-        fish[t0] = 1;
-        for( let d = 1; d <= numDays; d++ ){
-            let newFish = fish[0];
-            for( let i = 1; i < fish.length; i++ ){
-                fish[i-1] = fish[i];
-            }
-            fish[6] += newFish;
-            fish[8] = newFish;
-        }
-        // save result into cache before returning
-        return cache[key] = sum(fish);
+function numFish( t0, numDays){
+    fish = new Array(9).fill(0);
+    fish[t0] = 1;
+    for( let d = 0; d < numDays; d++ ){
+        fish[(d + 7) % fish.length] += fish[d % fish.length];
     }
-})();
+    return sum(fish);
+}
 
 function day_06a( numDays = 80){
-    let data = parseInput( Number, /,/);
-    return sum( data.map(numFish.bind( null, numDays)));
+    let data = parseInput( Number, /,/),
+        fish = Array.from( new Array(7), ( _, i) => numFish( i, numDays));
+    return sum( data.map( x => fish[x]));
 }
 
 function day_06b(){
