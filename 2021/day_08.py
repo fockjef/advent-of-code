e06ea1de8a5631e497abc16056863bcb3741aa60
jef <fockjef@gmail.com>
2021-12-08 22:25:06 -0600

    previous (?) was warranted, C-style comments no worky

--- 2021/day_08.py
+++ 2021/day_08.py
@@ -12,43 +12,43 @@ def decode(signalPatterns):
     patterns = signalPatterns[0:10]
     outputs = signalPatterns[-4:]
     digits = [None] * 10
-    // 1
+    # 1
     d = list(filter( lambda p: len(p) == 2, patterns))[0]
     patterns.remove(d)
     digits[1] = set(d)
-    // 7
+    # 7
     d = list(filter( lambda p: len(p) == 3, patterns))[0]
     patterns.remove(d)
     digits[7] = set(d)
-    // 4
+    # 4
     d = list(filter( lambda p: len(p) == 4, patterns))[0]
     patterns.remove(d)
     digits[4] = set(d)
-    // 8
+    # 8
     d = list(filter( lambda p: len(p) == 7, patterns))[0]
     patterns.remove(d)
     digits[8] = set(d)
-    // 9
+    # 9
     d = list(filter( lambda p: len(digits[4].intersection(p)) == 4, patterns))[0]
     patterns.remove(d)
     digits[9] = set(d)
-    // 0
+    # 0
     d = list(filter( lambda p: len(p) == 6 and len(digits[7].intersection(p)) == 3, patterns))[0]
     patterns.remove(d)
     digits[0] = set(d)
-    // 6
+    # 6
     d = list(filter( lambda p: len(p) == 6, patterns))[0]
     patterns.remove(d)
     digits[6] = set(d)
-    // 3
+    # 3
     d = list(filter( lambda p: len(digits[7].intersection(p)) == 3, patterns))[0]
     patterns.remove(d)
     digits[3] = set(d)
-    // 5
+    # 5
     d = list(filter( lambda p: len(digits[9].intersection(p)) == 5, patterns))[0]
     patterns.remove(d)
     digits[5] = set(d)
-    // 2
+    # 2
     digits[2] = set(patterns[0])
     return int("".join([str(digits.index(set(p))) for p in outputs]))
 
