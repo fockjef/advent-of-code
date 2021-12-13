b645e6cebe242b975f0ed1b738032b459edaaad2
fockjef <fockjef@users.noreply.github.com>
2021-12-13 11:30:58 -0600

    day 12 solved. not to self: don't use "g" flag when using test() method

--- 2021/day_12.js
+++ 2021/day_12.js
@@ -1,49 +1,38 @@
-const isUpper = /^[A-Z]+$/g;
+function isValidPath1(path){
+    return isUpperCase(path[0]) || !path.includes( path[0], 1);
+}
 
-function findPaths_broken( path, nodes){
+function isValidPath2(path){
+    let smallCaves = path.slice(1).filter(isLowerCase);
+    return isValidPath1(path) || smallCaves.length == new Set(smallCaves).size;
+}
+
+function findPaths( path, nodes, isValidPath){
     if( path[0] == "end" ){
-        return [path.reverse];
+        return [path.reverse()];
     }
     return [].concat(
         ...nodes[path[0]]
-        .filter( node => isUpper.test(node) || !path.includes(node))
-        .map( node => findPaths( [node, ...path], nodes))
+        .map( node => [ node, ...path])
+        .filter(isValidPath)
+        .map( path => findPaths( path, nodes, isValidPath))
     );
 }
 
-function findPaths( nodes){
-    let numPaths = 0,
-        paths = [["start"]],
-        nextPaths = [];
-    while( paths.length ){
-        for( let i = 0; i < paths.length; i++ ){
-            p = paths[i];
-            if( p[0] == "end" ){
-                numPaths++;
-            }
-            else{
-                nodes[p[0]]
-                .filter( node => isUpper.test(node) || !p.includes(node))
-                .forEach( node => {
-                    nextPaths.push([ node, ...p]);
-                })
-            }
-        }
-        paths = nextPaths;
-        nextPaths = [];
-    }
-    return numPaths;
-}
-
-function day_12a(){
+function day_12a( isValidPath = isValidPath1){
     let data = parseInput( x => x.split("-")),
         nodes = {};
     // intialize nodes
     data.forEach( ([n1,n2]) => {
         if( !(n1 in nodes) ) nodes[n1] = [];
         if( !(n2 in nodes) ) nodes[n2] = [];
-        nodes[n1].push(n2);
-        nodes[n2].push(n1);
+        if( n2 !== "start" ) nodes[n1].push(n2);
+        if( n1 !== "start" ) nodes[n2].push(n1);
     });
-    return findPaths( [["start"]], nodes);
+    nodes.end = [];
+    return findPaths( ["start"], nodes, isValidPath).length;
+}
+
+function day_12b(){
+    return day_12a(isValidPath2);
 }
