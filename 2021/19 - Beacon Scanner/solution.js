085eb82ada6de8799e8706f5c2f4aba69276f3f0
fockjef <fockjef@users.noreply.github.com>
2021-12-21 12:11:32 -0600

    day 19 done, still need to catch up from the weekend

--- 2021/19 - Beacon Scanner/solution.js
+++ 2021/19 - Beacon Scanner/solution.js
@@ -1,85 +1,89 @@
 /* --- Day 19: Beacon Scanner --- */
 
 function silver(){
-    let data = parseInput( x => x.split(/\n/).slice(1).map( xx => xx.split(",").map(Number)), /\n\n/);
-    for
+    let data = parseInput( x => x.split(/\n/).slice(1).map( xx => xx.split(",").map(Number)), /\n\n/),
+        scanners = normalizeAll(data);
+    // count unique beacons
+    return new Set([].concat(...scanners).map( s => s.join(","))).size;
 }
 
-let offset = Array.from( new Array(6), () => ({}));
-for( let i = 1, diff; i < data.length; i++ ){
-    let j = 0;
-    diff = data[0][j] - data[i][j >>> 1];
-    console.log(diff)
+function gold(){
+    let data = parseInput( x => x.split(/\n/).slice(1).map( xx => xx.split(",").map(Number)), /\n\n/),
+        scanners = normalizeAll(data);
+    // find max manhattan distance between scanners
+    return Math.max(...[].concat(...scanners.map( ( s1, i) => scanners.slice(i + 1).map( s2 =>
+        Math.abs(s1.offset[0] - s2.offset[0]) +
+        Math.abs(s1.offset[1] - s2.offset[1]) +
+        Math.abs(s1.offset[2] - s2.offset[2])
+    ))));
 }
 
-getScannerOffset( scanners[0], scanner[1]);
-
-function getScannerOffset( s1, s2){
-    let axis = [ 0, 1, 2],
-        offset = [],
-        a;
-    // match x-axis
-    a = s1.map( ([ x, y, z]) => x);
-    for( let i = 0, off; i < axis.length; i++){
-        off = hasCommonAxis( a, s2.map( (xyz) => xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("x");
-            offset.push(off);
-            axis.splice( i, 1);
-        }
-        off = hasCommonAxis( a, s2.map( (xyz) => -xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("-x");
-            offset.push(off);
-            axis.splice( i, 1);
+function normalizeAll(scanners){
+    let normalized = [scanners.shift()];
+    normalized[0].offset = [ 0, 0, 0];
+    for( let i = 0; i < normalized.length && scanners.length; i++ ){
+        let refScanner = normalized[i];
+        for( let j = 0; j < scanners.length; j++ ){
+            let s = normalizeScanner( refScanner, scanners[j]);
+            if( s ){
+                normalized.push(s);
+                scanners.splice( j , 1);
+                j--;
+            }
         }
     }
-    if( offset.length != 1 ) return undefined;
-    // match y-axis
-    a = s1.map( ([ x, y, z]) => y);
-    for( let i = 0, off; i < axis.length; i++){
-        off = hasCommonAxis( a, s2.map( (xyz) => xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("y");
-            offset.push(off);
-            axis.splice( i, 1);
-        }
-        off = hasCommonAxis( a, s2.map( (xyz) => -xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("-y");
-            offset.push(off);
-            axis.splice( i, 1);
-        }
-    }
-    if( offset.length != 2 ) return undefined;
-    // match z-axis
-    a = s1.map( ([ x, y, z]) => z);
-    for( let i = 0, off; i < axis.length; i++){
-        off = hasCommonAxis( a, s2.map( (xyz) => xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("z");
-            offset.push(off);
-            axis.splice( i, 1);
-        }
-        off = hasCommonAxis( a, s2.map( (xyz) => -xyz[axis[i]]));
-        if( off != undefined ){
-            console.log("-z");
-            offset.push(off);
-            axis.splice( i, 1);
-        }
-    }
-    if( offset.length != 3 ) return undefined;
-    return offset;
+    return normalized;
 }
 
-function hasCommonAxis( s1, s2){
+function normalizeScanner( refScanner, scanner){
+    let scannerAxes = [ 0, 1, 2].map( axis => scanner.map( xyz => xyz[axis])),
+        xAxis, yAxis, zAxis;
+    xAxis = findCommonAxis( refScanner.map( xyz => xyz[0]), scannerAxes);
+    if( !xAxis ) return undefined;
+    yAxis = findCommonAxis( refScanner.map( xyz => xyz[1]), scannerAxes);
+    if( !yAxis ) return undefined;
+    zAxis = findCommonAxis( refScanner.map( xyz => xyz[2]), scannerAxes);
+    if( !zAxis ) return undefined;
+    scanner = xAxis.map( ( x, i) => [ x, yAxis[i], zAxis[i]]);
+    scanner.offset = [ xAxis.offset, yAxis.offset, zAxis.offset];
+    return scanner;
+}
+
+// find common axis for a given reference axis
+// for each potential axis check both regular and reverse orientation
+function findCommonAxis( refAxis, scannerAxes){
+    for( let i = 0; i < scannerAxes.length; i++ ){
+        for( dir = 1; dir >= -1; dir -= 2 ){
+            let scanAxis = scannerAxes[i].map( n => dir * n),
+                offset = findAxisOffset( refAxis, scanAxis);
+            if( offset != undefined ){
+                scanAxis = scanAxis.map( n => n + offset);
+                scanAxis.offset = offset;
+                // remove common axis from axes list
+                scannerAxes.splice( i, 1);
+                return scanAxis;
+            }
+        }
+    }
+    return undefined;
+}
+
+// calculate position difference (distance) between every coordinate pair
+// quick and dirty, tries prevent double counting (might be buggy still)
+function findAxisOffset( axis1, axis2){
     let diff = {};
-    for( let i = 0; i < s1.length; i++ ){
-        for( let j = 0; j < s2.length; j++ ){
-            let d = s1[i] - s2[j];
-            if( !(d in diff) ) diff[d] = new Set();
-            diff[d].add(j);
-            if( diff[d].size >= 12 ) return d;
+    for( let i = 0; i < axis1.length; i++ ){
+        for( let j = 0; j < axis2.length; j++ ){
+            let d = axis1[i] - axis2[j];
+            if( !(d in diff) ) diff[d] = [];
+            diff[d].push([ i, j]);
+            if(
+                diff[d].length >= 12 &&
+                new Set(diff[d].map( ([ ii, jj]) => ii)).size >= 12 &&
+                new Set(diff[d].map( ([ ii, jj]) => jj)).size >= 12
+            ){
+                return d;
+            }
         }
     }
     return undefined;
