d6fd448166f67da6d9381ce396f37aeeb2547c3d
fockjef <fockjef@users.noreply.github.com>
2021-12-25 11:36:33 -0600

    day 25, calendar complete!

---
+++ 2021/25 - Sea Cucumber/solution.js
@@ -0,0 +1,32 @@
+/* --- Day 25: Sea Cucumber --- */
+
+function silver(){
+    let data = parseInput( x => x.split("")),
+        numSteps = 0;
+    while( step(data) ){
+        numSteps++;
+    }
+    return numSteps + 1;
+}
+
+function step(herd){
+    let herdMoved = false;
+    [
+        { cuke: ">", rOff: 0, cOff: 1},
+        { cuke: "v", rOff: 1, cOff: 0},
+    ].forEach( ({ cuke, rOff, cOff}) => {
+        let temp = herd.map( row => row.slice());
+        for( let row = 0; row < herd.length; row++ ){
+            for( let col = 0; col < herd[0].length; col++ ){
+                let nextR = (row + rOff) % herd.length,
+                    nextC = (col + cOff) % herd[0].length;
+                if( temp[row][col] == cuke && temp[nextR][nextC] == "." ){
+                    herd[row][col] = ".";
+                    herd[nextR][nextC] = cuke;
+                    if( !herdMoved ) herdMoved = true;
+                }
+            }
+        }
+    })
+    return herdMoved;
+}
