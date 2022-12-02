c564b3993e1cb2fbe04a48bd0b06648b1eb99af4
jef <fockjef@gmail.com>
2022-12-02 13:42:37 -0600

    Day 2 javascript and python solutions

---
+++ 2022/02 - Rock Paper Scissors/solution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# --- Day 2: Rock Paper Scissors ---
+
+input = open("input.txt")
+data = [ x.strip() for x in input.readlines() ]
+input.close()
+
+strategy = {
+    "silver": {"A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6},
+    "gold":   {"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7},
+}
+
+def calcScore(strategyId):
+    score = 0
+    for round in data:
+        score = score + strategy[strategyId][round]
+    return score
+
+silver = lambda: calcScore("silver")
+gold = lambda: calcScore("gold")
