aa7feb5061d65e2cce65a6d8a1c8fb477f1566d8
jef <fockjef@gmail.com>
2022-12-04 20:33:13 -0600

    A few minor fixes

--- 2022/04 - Camp Cleanup/solution.js
+++ 2022/04 - Camp Cleanup/solution.js
@@ -1,7 +1,7 @@
 /* --- Day 4: Camp Cleanup --- */
 
-const silver = () => parseInput(x=>x.match(/\d+/g)).filter(isSuperset).length;
-const gold = () => parseInput(x=>x.match(/\d+/g)).filter(hasOverlap).length;
+var silver = () => parseInput(x=>x.match(/\d+/g)).filter(isSuperset).length;
+var gold = () => parseInput(x=>x.match(/\d+/g)).filter(hasOverlap).length;
 
 const isSuperset = ([lo1,hi1,lo2,hi2]) => (+lo1 <= lo2 && +hi1 >= hi2) || (+lo1 >= lo2 && +hi1 <= hi2);
 const hasOverlap = ([lo1,hi1,lo2,hi2]) => (+lo1 <= hi2 && +hi1 >= lo2);
--- 2022/04 - Camp Cleanup/solution.py
+++ 2022/04 - Camp Cleanup/solution.py
@@ -4,19 +4,20 @@
 with open("input.txt") as input:
     data = [ x.strip() for x in input.readlines() ]
 
-def countOverlap(method = "silver"):
+def countOverlap(method):
     count = 0
     for assignment in data:
         range1, range2 = assignment.split(",")
         lo1, hi1 = map( int, range1.split("-"))
         lo2, hi2 = map( int, range2.split("-"))
-        if method == "silver":
-            if (lo1 <= lo2 and hi1 >= hi2) or (lo2 <= lo1 and hi2 >= hi1):
-                count = count + 1
-        else:
-            if lo1 <= hi2 and hi1 >= lo2:
-                count = count + 1
+        match method:
+                case "superset":
+                    if (lo1 <= lo2 and hi1 >= hi2) or (lo2 <= lo1 and hi2 >= hi1):
+                        count = count + 1
+                case "intersect":
+                    if lo1 <= hi2 and hi1 >= lo2:
+                        count = count + 1
     return count
 
-silver = lambda: countOverlap()
-gold = lambda: countOverlap("gold")
+silver = lambda: countOverlap("superset")
+gold = lambda: countOverlap("intersect")
