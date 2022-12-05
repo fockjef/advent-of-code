ba0917e0a382cac52909dcec577e6e2f503f2bf8
fockjef <fockjef@users.noreply.github.com>
2022-12-04 19:38:32 -0600

    day 4 done

---
+++ 2022/04 - Camp Cleanup/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# --- Day 4: Camp Cleanup ---
+
+with open("input.txt") as input:
+    data = [ x.strip() for x in input.readlines() ]
+
+def countOverlap(method = "silver"):
+    count = 0
+    for assignment in data:
+        range1, range2 = assignment.split(",")
+        lo1, hi1 = map( int, range1.split("-"))
+        lo2, hi2 = map( int, range2.split("-"))
+        if method == "silver":
+            if (lo1 <= lo2 and hi1 >= hi2) or (lo2 <= lo1 and hi2 >= hi1):
+                count = count + 1
+        else:
+            if lo1 <= hi2 and hi1 >= lo2:
+                count = count + 1
+    return count
+
+silver = lambda: countOverlap()
+gold = lambda: countOverlap("gold")
