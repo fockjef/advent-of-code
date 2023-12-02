1b0c8f0179e7c954c73d369b276fe5c7a4241f37
fockjef <fockjef@users.noreply.github.com>
2023-12-02 17:13:03 -0600

    Reworked day 1 part 2

--- 2023/01 - Trebuchet!/solution.js
+++ 2023/01 - Trebuchet!/solution.js
@@ -7,23 +7,15 @@ function silver() {
 }
 
 function gold() {
-    let nums = {
-            one: 1,
-            two: 2,
-            three: 3,
-            four: 4,
-            five: 5,
-            six: 6,
-            seven: 7,
-            eight: 8,
-            nine: 9
-        },
-        numsRE = new RegExp(Object.keys(nums).join('|'));
-    return parseInput(x => {
-        while (numsRE.test(x)) {
-            x = x.replace(numsRE, n => nums[n] + n.slice(1));
-        }
-        x = x.match(/\d/g);
-        return x[0] + x[x.length - 1];
-    }).sum();
+    let nums = "one|two|three|four|five|six|seven|eight|nine",
+        numRE = new RegExp(nums),
+        lastNumRE = new RegExp(`^(.*)(${nums})`);
+    nums = Object.fromEntries(nums.split("|").map((n, i) => [n, i + 1]));
+    return parseInput(x => x
+            .replace(numRE, n => nums[n] + n.slice(1))
+            .replace(lastNumRE, (_, pre, n) => pre + nums[n])
+            .match(/\d/g)
+        )
+        .map(d => d[0] + d[d.length - 1])
+        .sum();
 }
