34c4212f9a5b4bd68d85a22475d145a6bbac1695
fockjef <git@fockjef.net>
2023-12-12 09:04:18 -0600

    Need to catch up on backlog

--- 2023/09 - Mirage Maintenance/solution.js
+++ 2023/09 - Mirage Maintenance/solution.js
@@ -1,23 +1,33 @@
 /* --- Day 9: Mirage Maintenance --- */
 
-function silver(){
-    return parseInput(x => x.split(/ +/).map(Number)).map(nextValue).sum();
+function silver() {
+    return parseInput(x => x.split(/ +/).map(Number))
+        .map(nextValue)
+        .sum();
 }
 
-function gold(){
-    return parseInput(x => x.split(/ +/).map(Number)).map(prevValue).sum();
+function gold() {
+    return parseInput(x => x.split(/ +/).map(Number))
+        .map(prevValue)
+        .sum();
 }
 
-function nextValue(sequence){
-    if( sequence.every(val => val == sequence[0]) ){
+function nextValue(sequence) {
+    if (sequence.every(val => val == sequence[0])) {
         return sequence[0];
     }
-    return sequence[sequence.length-1] + nextValue(sequence.slice(1).map((val ,i) => val - sequence[i]));
+    return (
+        sequence[sequence.length - 1] +
+        nextValue(sequence.slice(1).map((val, i) => val - sequence[i]))
+    );
 }
 
-function prevValue(sequence){
-    if( sequence.every(val => val == sequence[0]) ){
+function prevValue(sequence) {
+    if (sequence.every(val => val == sequence[0])) {
         return sequence[0];
     }
-    return sequence[0] - prevValue(sequence.slice(1).map((val ,i) => val - sequence[i]));
+    return (
+        sequence[0] -
+        prevValue(sequence.slice(1).map((val, i) => val - sequence[i]))
+    );
 }
