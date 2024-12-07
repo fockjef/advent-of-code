a48eac3b068a8261fa437627369994126dbbf26d
fockjef <fockjef@hillock>
2024-12-07 10:35:49 -0600

    Adds day 7 perl and improves original day 7 solution

--- 2024/07 - Bridge Repair/solution.js
+++ 2024/07 - Bridge Repair/solution.js
@@ -1,26 +1,23 @@
 /* --- Day 7: Bridge Repair --- */
 
 function silver(numOps = 2){
-    return parseInput(x => x.match(/\d+/g).map(Number)).filter(eq => testEquation(numOps, ...eq)).map(eq => eq[0]).sum();
+    return parseInput(x => x.match(/\d+/g).map(Number)).filter(testEquation.bind({}, numOps)).map(x => x[0]).sum();
 }
 
 function gold(){
     return silver(3);
 }
 
-function testEquation(numOps, goal, ...values){
-    for(let i = 0; i < Math.pow(numOps, values.length-1); i++){
-        let ops = i,
-            res = values[0];
-        for(let j = 1; j < values.length; j++){
-            switch(ops % numOps){
-                case 0: res += values[j]; break;
-                case 1: res *= values[j]; break;
-                default: res = Number(`${res}${values[j]}`);
-            }
-            ops = (ops / numOps) >>> 0;
-        }
-        if(res == goal) return true;
+function testEquation(numOps, values){
+    let [target, acc] = values;
+
+    function compute(acc, idx){
+        if(acc > target) return false;
+        if(idx == values.length) return acc == target;
+        return compute(acc * values[idx], idx + 1) ||
+               compute(acc + values[idx], idx + 1) ||
+               (numOps > 2 && compute(acc * Math.pow(10, Math.ceil(Math.log10(values[idx] + .5))) + values[idx], idx + 1));
     }
-    return false;
+
+    return compute(acc, 2);
 }
--- 2024/aoc-perl.sh
+++ 2024/aoc-perl.sh
@@ -1,14 +1,27 @@
+# Day 1
 perl -ape'($A[$.],$B[$.-1])=@F}{@A=sort@A;$\+=abs$_-shift@A for sort@B' input.txt
 perl -ape'($A[$.],$B[$.])=@F}{for$a(@A){$\+=$a*grep{$a==$_}@B}' input.txt
 
+# Day 2
 perl -paXe'sub S{!grep{!(($_[0]<=>$_[1])*($_[-2]-pop@_)~~[1..3])}1..$#_}$\+=S@F}{' input.txt
 perl -paXe'sub S{!!grep{@x=@_;splice@x,$_,1;!grep{!(($x[0]<=>$x[1])*($x[-2]-pop@x)~~[1..3])}1..$#x}0..$#_}$\+=S@F}{' input.txt
 
+# Day 3
 perl -p0e'$\+=eval y/,/*/rfor/mul\((\d+,\d+)\)/g}{' input.txt
 perl -p0e's/don\x27t\(\).*?(do\(\)|$)//sg;$\+=eval y/,/*/rfor/mul\((\d+,\d+)\)/g}{' input.txt
 
+# Day 4
 perl -pe 'push@G,["",/./g]}{for$r(0..$.){for$c(0..$.){$_=join"",map{$G[$r][$c+$_]||0,$G[$r+$_][$c]||0,$G[$r+$_][$c+$_]||0,$G[$r+$_][$c-$_]||0}(0..3);$_=$n+=s/X(?=...M...A...S)|S(?=...A...M...X)//g}}' input.txt
 perl -pe 'push@G,[/./g]}{for$r(0..$.){$\+=("$G[$r+1][$_+1]$G[$r][$_]$G[$r][$_+2]$G[$r+2][$_+2]$G[$r+2][$_]"=~/A(MMSS|MSSM|SMMS|SSMM)/)for(0..$.)}' input.txt
 
+# Day 5
 perl -00pe '(@R?@U:@R)=/\S+/g}{$\+=substr$_,y///c/2-1for grep{$u=" $_";!grep{($a,$b)=map{index$u,$_}/\d+/g;$a/$b>1}@R}@U' input.txt
 perl -00pe '(@R?@U:@R)=/\S+/g}{for(@U){$_=$u=" $_,";for(sort@R){($a,$b)=map{index$u,$_}/\d+/g;substr$u,$b,0,substr$u,$a,3,""if$a/$b>1}$\+=substr$u,y///c/2-1if$u ne$_}' input.txt
+
+# Day 6
+perl -e 'print "grid problems are dumb"' input.txt
+perl -e 'print "grid problems are dumb"' input.txt
+
+# Day 7
+perl -pe 'push@X,[/\d+/g]}{sub C{my($g,$a,$u,@v)=@_;$a>$g?0:$u?C($g,$a*$u,@v)||C($g,$a+$u,@v):$a==$g}$\+=@$_[0]*C@$_ for@X' input.txt
+perl -pe 'push@X,[/\d+/g]}{sub C{my($g,$a,$u,@v)=@_;$a>$g?0:$u?C($g,$a*$u,@v)||C($g,$a+$u,@v)||C($g,$a.$u,@v):$a==$g}$\+=@$_[0]*C@$_ for@X' input.txt
